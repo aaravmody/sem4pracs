cc82f2b449cbf95cec64818f8c28be57b7c1bfff
aaravmody <aaravmody2@gmail.com>
2024-05-12 02:18:52 +0530

    OS all codes done

--- OS/CPU Scheduling/priorityNP.c
+++ OS/CPU Scheduling/priorityNP.c
@@ -1,81 +1,70 @@
-#include<stdio.h>
-#include<limits.h>
-void main()
-{
-    int ct[100],tat[100],wt[100],n=7,i,j,temp,temp1,temp2,ttat=0,twt=0,ind=0,cgant[7];
-    int gant[7];
-    float attat,awt;
-    int at[7]={0,1,2,3,4,5,6};
-    int bt[7]={4,2,3,5,1,4,6};
-    int pr[7]={2,4,6,10,8,12,9};
-    int v[7]={0,0,0,0,0,0,0};
-    int pro[7]={1,2,3,4,5,6,7};
-    for(i=0;i<n-1;i++)
-    {
-        for(j=0;j<n-1;j++)
-        {
-            if(pr[j]>pr[j+1])
-            {
-                temp=pr[j];
-                pr[j]=pr[j+1];
-                pr[j+1]=temp;                
-                temp=at[j];
-                at[j]=at[j+1];
-                at[j+1]=temp;
-                temp1=bt[j];
-                bt[j]=bt[j+1];
-                bt[j+1]=temp1;
-                temp2=pro[j];
-                pro[j]=pro[j+1];
-                pro[j+1]=temp2;
-            }
+#include <stdio.h>
+#include <stdlib.h>
+
+struct process{
+    int arrival, burst, completion, turnaround, waiting, completed, priority;
+};
+struct process pq[10];
+
+int getHighPriorityProcess(int clock, int total){
+    int highPriorityProcess;
+    for (int i=0; i<total; i++){
+        if (pq[i].arrival <= clock && pq[i].completed != 1){
+            highPriorityProcess = i;
+            break;
         }
     }
-    i=0;
-    int flag=0;
-    while(flag!=7)
-    {
-        int f=0;
-        for(j=0;j<n;j++)
-        {
-            if(at[j]<=i && v[j]==0)
-            {
-                ct[j]=i+bt[j];
-                v[j]=1;
-                i=ct[j];
-                flag++;
-                f++;
-                gant[ind]=pro[j];
-                cgant[ind]=ct[j];
-                ind++;
-                break;
-            }
+    for(int i=highPriorityProcess; i<total; i++){
+        if (pq[i].arrival <= clock && pq[i].completed != 1 && pq[i].priority > pq[highPriorityProcess].priority){
+            highPriorityProcess = i;
         }
-        if(f==0)
-        i++;
     }
-    for(i=0;i<n;i++)
-    {
-        tat[i]=ct[i]-at[i];
-        wt[i]=tat[i]-bt[i];
-    }
-    printf("Process\tprio\tAT\tBT\tCT\tTAT\tWT");
-    for(i=0;i<n;i++)
-    {
-        ttat+=tat[i];
-        twt+=wt[i];
-    }
-    awt=(float)twt/n;
-    attat=(float)ttat/n;
-    for(i=0;i<n;i++)
-    printf("\n%d\t%d\t%d\t%d\t%d\t%d\t%d\n",pro[i],pr[i],at[i],bt[i],ct[i],tat[i],wt[i]);
-    printf("\nAvg TAT=%f\nAvg WT=%f",attat,awt);
-    printf("\nGant Chart \n");
-    for(i=0;i<ind;i++)
-    printf("%d\t",gant[i]);
-    printf("\n");
-    for(i=0;i<ind;i++)
-    printf("%d\t",cgant[i]);
+    return highPriorityProcess;
 }
 
+void main(){
+    int total, clock=0, completedProcess=0;
+    float avgTAT=0, avgWT=0;
+    printf("This is Non Preemtive Priority process sheduling\n");
+    printf("Enter number of processes : ");
+    scanf("%d",&total);
+    
+    for (int i=0; i<total; i++){
+        printf("Enter process arrival time, burst time, priority : ");
+        scanf("%d%d%d",&pq[i].arrival,&pq[i].burst,&pq[i].priority);
+        pq[i].completed = 0;
+    }
+    for(int i=1; i<total; i++){
+        int j = i-1;
+        struct process key = pq[i];
+        while(j >= 0 && key.arrival < pq[j].arrival){
+            struct process temp = pq[j];
+            pq[j+1] = pq[j];
+            pq[j] = temp;
+            j--;
+        }
+        pq[j+1] = key;
+    }
 
+    clock = pq[0].arrival;
+    while(completedProcess < total){
+        int process = getHighPriorityProcess(clock, total);
+        pq[process].completion = clock + pq[process].burst;
+        pq[process].turnaround = pq[process].completion - pq[process].arrival;
+        pq[process].waiting = pq[process].turnaround - pq[process].burst;
+        clock += pq[process].burst;
+        completedProcess += 1;
+        pq[process].completed = 1;
+    }
+    
+    
+    printf("Id\tAT\tBT\tCT\tTAT\tWT\n");
+    for(int i=0; i<total; i++){
+        avgTAT += pq[i].turnaround;
+        avgWT += pq[i].waiting;
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n", i, pq[i].arrival, pq[i].burst, pq[i].completion, pq[i].turnaround, pq[i].waiting);
+    }
+    avgTAT /= total; 
+    avgWT /= total; 
+    printf("Avg TAT : %.2f, Avg WT : %.2f", avgTAT, avgWT);
+}
