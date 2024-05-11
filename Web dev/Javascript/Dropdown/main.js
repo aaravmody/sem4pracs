7fe856dfb8f0520a1db425b5a8eb6bbbef2c95cd
aaravmody <aaravmody2@gmail.com>
2024-05-11 16:46:44 +0530

    Web dev

---
+++ Web dev/Javascript/Dropdown/main.js
@@ -0,0 +1,9 @@
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdownContent");
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+}
+
